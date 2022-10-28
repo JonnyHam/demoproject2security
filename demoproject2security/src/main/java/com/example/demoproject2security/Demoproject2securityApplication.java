b933ce96b7595d223fc3a06a607384ec7600e792
JonnyHam <jonajonkim@gmail.com>
2022-10-28 10:35:48 -0500

    Initial commit

---
+++ demoproject2security/src/main/java/com/example/demoproject2security/Demoproject2securityApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoproject2security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Demoproject2securityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoproject2securityApplication.class, args);
+	}
+
+}
---
+++ demoproject2security/src/test/java/com/example/demoproject2security/Demoproject2securityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoproject2security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoproject2securityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
